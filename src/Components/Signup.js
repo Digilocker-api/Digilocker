182eb1ed9aab694f9989503a9fe74f26a91ad126
Sakshee <sakshee1120@gmail.com>
2020-10-15 13:38:02 +0530

    Form complete

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './signcard.css';
 
 const Signup = () => {
+
+    const [name, setName] = useState('')
+    const [date, setDate] = useState('')
+    const [month, setMonth] = useState('')
+    const [year, setYear] = useState('')
+    const [phone, setPhone] = useState('')
+    const [pin, setPin] = useState('')
+    const [email, setEmail] = useState('')
+    const [aadhar, setAadhar] = useState('')
+    
+    const submitForm = e => {
+        alert("Current state is : " + JSON.stringify({name, date, month, year, phone, 
+                                                      pin, email, aadhar}))
+        console.log("Current state is : " + JSON.stringify({name, date, month, year, phone, 
+                                                      pin, email, aadhar}))
+          
+        e.preventDefualt()
+    }
+
     return(
         <>
         <div className="d-flex justify-content-center">
@@ -10,25 +29,28 @@ const Signup = () => {
             <div className="card-body">
                 <h5 className="card-title text-center">Create a new account</h5>
                 <div className="form-wrap">
-                <form>
+                <form onSubmit={submitForm}>
                     <div className="form-group">
-                        <input type="text" value="Full name" className="form-control"/>
+                        <input type="text" placeholder="Full name" className="form-control"
+                        onChange={ e => setName(e.target.value) }/>
                         <small id="Fullname" class="form-text text-muted">
                             Please enter your full name as per Aadhar
                         </small>
                     </div>
                     <div className="form-group">
                         <label for="date">Date of Birth</label>
-                        <div className="form-row">
-
+                        <div className="form-row" id="DOB">
                             <div className="form-group col-sm-3">
-                                <input type="text" className="form-control" id="date" value="Date"/>
+                                <input type="text" className="form-control" id="date" placeholder="Date"
+                                onChange={ e => setDate(e.target.value) }/>
                             </div>
                             <div className="form-group col-sm-4">
-                                <input type="text" className="form-control" id="month" value="Month"/>
+                                <input type="text" className="form-control" id="month" placeholder="Month"
+                                onChange={ e => setMonth(e.target.value) }/>
                             </div>
                             <div className="form-group col-sm-5">
-                                <input type="text" className="form-control" id="year" value="Year"/>
+                                <input type="text" className="form-control" id="year" placeholder="Year"
+                                onChange={ e => setYear(e.target.value) }/>
                             </div>
                         </div>
                         <small id="DOB" class="form-text text-muted">
@@ -42,19 +64,58 @@ const Signup = () => {
                                 <option>Female</option>
                         </select>
                     </div>
-                    <div className="form-group form-inline">        
-                        <div className="input-group-prepend">
-                            <span className="input-group-text">+91</span>
+
+                    <div className="form-group">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroupPrepend">+91</span>
                         </div>
-                        <input type="text" className="form-control" id="phone" value="Phone number"/>
+                        <input type="text" class="form-control" id="validationCustomUsername"
+                        placeholder="Phone" aria-describedby="inputGroupPrepend" required
+                        onChange={ e => setPhone(e.target.value) }/>
                     </div>
                     <small id="mob" class="form-text text-muted">
                         We will use this mobile for future communications
                     </small>
+                    </div>    
+
+                    <div className="form-group">
+                        <input type="password" placeholder="Set 6-digit security PIN" className="form-control"
+                        onChange={ e => setPin(e.target.value) }/>
+                        <small id="Fullname" class="form-text text-muted">
+                            6 digit PIN provides extra security to your account with two factor authentication
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <input type="email" id="email" placeholder="E-mail ID" className="form-control"
+                        required onChange={ e => setEmail(e.target.value) }/>
+                        <small id="Fullname" class="form-text text-muted">
+                            We will use this Email for future communications
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <div className="input-group">
+                        <input type="text" placeholder="Aadhar number" className="form-control"
+                        onChange={ e => setAadhar(e.target.value) }/>
+                        {/*<div class="input-group-append">
+                            <span class="input-group-text">
+                                <image style={{height: 20}} alt="aadharlogo"
+                                 src={require('../Images/Aadhaar-Logo-PNG.jpg')}/>
+                            </span>
+                        </div>*/}
+                        </div>
+                        <small id="Fullname" class="form-text text-muted">
+                            Digilocker uses Aadhar to verify identity of the user and also to enable authentic
+                            document access
+                        </small>
+                    </div>
                 </form>
                 </div>
                 <div className="text-center">
-                <Link to="/signup" className="btn btn-primary">Sign up</Link>
+                <button className="btn btn-primary" type="submit" id="submit" value="submit">
+                    Button
+                </button>
+                {/*<Link to="/signup" className="btn btn-primary" type="submit">Sign up</Link>*/}
                 </div>
             </div>
             </div>
